02ed5791e5b9876a507c77b61d6a4e7878520cb1
Shannon Moeller <me@shannonmoeller.com>
2021-06-08 22:21:32 +0000

    simplify test util

--- test/utils/test.js
+++ test/utils/test.js
@@ -1,22 +1,15 @@
 'use strict';
 
 var assert = require('assert');
-var tests = [];
 
-function test(msg, fn) {
-	tests.push([msg, fn]);
-}
-
-setTimeout(async function run() {
-	for (const [msg, fn] of tests) {
-		try {
-			await fn(assert);
-			console.log(`pass - ${msg}`);
-		} catch (error) {
-			console.error(`fail - ${msg}`, error);
-			process.exit(1);
-		}
+async function test(msg, fn) {
+	try {
+		await fn(assert);
+		console.log(`pass - ${msg}`);
+	} catch (error) {
+		console.error(`fail - ${msg}`, error);
+		process.exit(1);
 	}
-});
+}
 
 module.exports = test;
